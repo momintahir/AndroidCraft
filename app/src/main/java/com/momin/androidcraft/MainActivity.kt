6a4a9936c4370d02e3cd45d34f6d204f71e63fe2
Momin Tahir <momintahir7@gmail.com>
2024-08-01 22:08:38 +0500

    imports cleanup

--- app/src/main/java/com/momin/androidcraft/MainActivity.kt
+++ app/src/main/java/com/momin/androidcraft/MainActivity.kt
@@ -3,9 +3,6 @@ package com.momin.androidcraft
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.momin.androidcraft.ui.theme.AndroidCraftTheme
 
 class MainActivity : ComponentActivity() {
